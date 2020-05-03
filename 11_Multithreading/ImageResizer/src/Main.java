1d44878626ec9338e94e17f3332a565ce170b5fe
@gorbachiov_aleksei <alewbrex@gmail.com>
2020-05-03 13:21:52 +0600

    ImageResizer - done(refresh)

--- 11_Multithreading/ImageResizer/src/Main.java
+++ 11_Multithreading/ImageResizer/src/Main.java
@@ -8,37 +8,39 @@ public class Main
     {
         String srcFolder = "C:/Users/aleks/Desktop/FirstDirectory";
         String dstFolder = "C:/Users/aleks/Desktop/SecondDirectory";
+
         File srcDir = new File(srcFolder);
         long start = System.currentTimeMillis();
         File[] files = srcDir.listFiles();
         int processor = Runtime.getRuntime().availableProcessors();
-
+        boolean more = files.length > processor;
         int oneToEight = files.length / processor;
+        int onePlus = oneToEight + 1;
+        int mdl = files.length % processor;
 
-        for (int i = 0; i < processor; i++) {
-            File[] files1 = new File[oneToEight];
-            System.arraycopy(files, (oneToEight * i), files1, 0, files1.length);
-            new Thread(new ImageResizer(files1, newWidth, dstFolder,start)).start();
+        if (more) {
+            for (int g = 0; g < processor; g++) {
+                if (g < mdl) {
+                    File[] files1 = new File[onePlus];
+                    System.arraycopy(files,(onePlus * g), files1, 0, files1.length);
+                    ImageResizer resizer = new ImageResizer(files1, newWidth, dstFolder,start);
+                    new Thread(resizer).start();
+                }
+                else {
+                    File[] files1 = new File[oneToEight];
+                    System.arraycopy(files,(((oneToEight - 1)*mdl) + (oneToEight * g)), files1, 0, files1.length);
+                    ImageResizer resizer = new ImageResizer(files1, newWidth, dstFolder,start);
+                    new Thread(resizer).start();
+                }
+            }
         }
-
-//        File[] files1 = new File[quarter];
-//        System.arraycopy(files, 0, files1, 0, files1.length);
-//        ImageResizer resizer1 = new ImageResizer(files1, newWidth, dstFolder,start);
-//        new Thread(resizer1).start();
-//
-//        File[] files2 = new File[quarter];
-//        System.arraycopy(files, quarter, files2, 0, files2.length);
-//        ImageResizer resizer2 = new ImageResizer(files2, newWidth, dstFolder,start);
-//        new Thread(resizer2).start();
-//
-//        File[] files3 = new File[quarter];
-//        System.arraycopy(files, files.length - (files.length/2), files3, 0, files3.length);
-//        ImageResizer resizer3 = new ImageResizer(files3, newWidth, dstFolder,start);
-//        new Thread(resizer3).start();
-//
-//        File[] files4 = new File[quarter];
-//        System.arraycopy(files, files.length - quarter, files4, 0, files4.length);
-//        ImageResizer resizer4 = new ImageResizer(files4, newWidth, dstFolder,start);
-//        new Thread(resizer4).start();
+        else {
+            for (int f = 0; f < files.length ; f++) {
+                File[] files1 = new File[1];
+                System.arraycopy(files, f, files1, 0, files1.length);
+                ImageResizer resizer = new ImageResizer(files1, newWidth, dstFolder,start);
+                new Thread(resizer).start();
+            }
+        }
     }
 }
